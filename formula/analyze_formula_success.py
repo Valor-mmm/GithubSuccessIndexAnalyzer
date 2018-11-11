596c5bb7d635639ac0446eda7eb73b8c0bd34f67
valor-mmm <stefan1.hauser@st.oth-regensburg.de>
2018-11-11 20:52:16 +0100

    Developing a formula to calculate the success of a repository.

--- formula/analyze_formula_success.py
+++ formula/analyze_formula_success.py
@@ -1,8 +1,13 @@
 from matplotlib import pyplot as plt
+from numpy import arange
+
 import pandas as pd
 
 df = pd.read_pickle('formula_analysis_df.pkl')
-
+del df['formula3']
 plt.figure()
 df.plot()
+plt.xticks(arange(5), list(df.index), rotation=25)
+plt.tight_layout()
+plt.savefig('formulaAnalysis.png', dpi=400)
 plt.show()
